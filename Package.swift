e1eed06b9f7ca024d19996e1df28ad84df5c4600
dimim <marios@adjust.com>
2026-04-16 10:40:53 +0200

    [spm] Promote v3.67.0 from beta to release

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "AdjustSignature",
             // swiftlint:disable line_length
-            url: "https://github.com/adjust/adjust_signature_sdk/releases/download/v3.67.0-beta/AdjustSigSdk-iOS-tvOS-Dynamic-3.67.0.xcframework.zip",
+            url: "https://github.com/adjust/adjust_signature_sdk/releases/download/v3.67.0/AdjustSigSdk-iOS-tvOS-Dynamic-3.67.0.xcframework.zip",
             // swiftlint:enable line_length
             checksum: "0222b91d55f569a801f3bbb2c2a2d85bd7a62a835811a23bcb1b5052e937301c"
         )
